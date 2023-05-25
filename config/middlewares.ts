74e761c1d3b4a6b4f994ee8cb7b3f7e80dd0680a
Diego Alvarado Otey <d.alvaradotey@gmail.com>
2023-05-24 21:58:41 -0400

    ADD: s3 plugins

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,33 @@
 export default [
   'strapi::errors',
-  'strapi::security',
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'anyaeco.s3.us-west-2.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'anyaeco.s3.us-west-2.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
